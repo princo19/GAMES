64412659c151ec9fca40311f84db32c7f88d9524
princo19 <64467869+princo19@users.noreply.github.com>
2020-05-03 21:40:27 +0530

    initial commit

---
+++ snake1.js
@@ -0,0 +1,151 @@
+function init(){
+	canvas = document.getElementById("mycanvas");
+    W = canvas.width= 1000 ;
+	H = canvas.height = 1000;
+	pen = canvas.getContext('2d');
+	cs=66;
+	food = getRandomFood();
+	game_over=false;
+	food_img = new Image();
+	food_img.src = "apple.jpg";
+	trophy_img = new Image();
+	trophy_img.src = "trophy.jpg";
+	score = 5;
+
+	snake = {
+		init_len:5,
+		color:"red",
+		cells:[],
+		direction:"right",
+		
+		createSnake:function(){
+			for( var i=this.init_len;i>0;i--){
+				this.cells.push({x:i,y:0});
+			}
+		},
+		drawSnake:function(){
+			for(var i=0; i<this.cells.length;i++){
+				pen.fillStyle = this.color;
+                pen.fillRect(this.cells[i].x*cs,this.cells[i].y*cs,cs-2,cs-2);
+
+		    }
+
+		},
+		updateSnake:function(){
+			//console.log("updating snake according ")
+			var headX= this.cells[0].x;
+			var headY= this.cells[0].y;
+			if(headX==food.x && headY==food.y){
+				food = getRandomFood();
+				score ++ ;
+			}
+			else
+			{
+				this.cells.pop();
+
+			}
+
+
+			
+			
+			var nextX,nextY;
+
+			if(this.direction=="right"){
+				nextX=headX + 1;
+				nextY = headY;
+			}else if(this.direction=="left"){
+				nextX=headX - 1;
+				nextY = headY;
+			}else if(this.direction=="down"){
+				nextX=headX;
+				nextY = headY + 1;
+			}else{
+				nextX=headX;
+				nextY = headY - 1;
+			}
+
+			this.cells.unshift({x:nextX,y:nextY});
+
+			//prevent from going out
+			var last_x = Math.round(W/cs);
+			var last_y = Math.round(H/cs);
+
+			if(this.cells[0].y<0 || this.cells[0].x<0 || this.cells[0].x>last_x || this.cells[0].y>last_y){
+				game_over=true;
+			}
+
+		}
+	};
+
+	snake.createSnake();
+	function keyPressed(e){
+		if(e.key=="ArrowRight"){
+			snake.direction= "right";
+		}
+		else if(e.key=="ArrowLeft"){
+			snake.direction = "left";
+		}
+		else if (e.key=="ArrowDown"){
+			snake.direction = "down";
+		}
+		else{
+			snake.direction ="up";
+		}
+		console.log(snake.direction);
+
+	}
+	document.addEventListener('keydown',keyPressed);
+
+	
+}
+
+
+function draw(){
+	pen.clearRect(0,0,W,H);
+	snake.drawSnake();
+	pen.fillStyle="green";
+	pen.drawImage(food_img,food.x*cs,food.y*cs,cs,cs);
+	pen.drawImage(trophy_img,18,20,cs,cs);
+	pen.fillStyle="red";
+	pen.font = "50px Roboto";
+	pen.fillText(score,50,50);
+	
+}
+function update (){
+	snake.updateSnake();
+
+
+}
+
+function getRandomFood(){
+	var foodX = Math.round(Math.random()*(W-cs)/cs);
+	var foodY = Math.round(Math.random()*(H-cs)/cs);
+    var food = {
+       x:foodX,
+       y:foodY,
+       color:"red",
+    }
+    return food
+    
+}	
+	
+
+
+function gameloop(){
+	if(game_over==true){
+		clearInterval(f);
+		alert("Game Over");
+		return;
+	}
+
+    draw();
+	update();
+	
+    
+}  
+
+
+init();
+
+var f =setInterval(gameloop,100);
+
